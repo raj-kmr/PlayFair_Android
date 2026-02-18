5630341c688c1e96ddb5920692ca0740e0fcc649
Raj <raj3373y@gmail.com>
2026-02-18 23:47:37 +0530

    Improve understanding by adding comments

--- features/auth/Signup.tsx
+++ features/auth/Signup.tsx
@@ -3,14 +3,43 @@ import { useState } from "react";
 import { Button, ScrollView, StyleSheet, Text, TextInput } from "react-native";
 
 export const Signup = () => {
+  // local states for form inputs
   const [email, setEmail] = useState("");
   const [name, setName] = useState("");
   const [password, setPassword] = useState("");
 
+  // Sends signup request to backend to create new account
   const handleSignUp = async () => {
-    console.log("button pressed");
-    await SecureStore.setItemAsync("email", email);
-    console.log("Key value saved");
+    try {
+      console.log(email, name, password)
+      const res = await fetch("http://192.168.1.13:3000/auth/signup", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+          username: name,
+          email,
+          password
+        })
+      })
+      
+      const data = await res.json();
+      console.log("Response", data);
+  
+      if(!res.ok){
+        alert(data.message || "Signup failed")
+        return;
+      }
+  
+      alert("Signup succssful")
+    } catch(err) {
+      console.log("Signup error:  ", err)
+      alert("Network error")
+    }
+
+    // await SecureStore.setItemAsync("email", email);
+    // console.log("Key value saved");
   };
 
   return (
