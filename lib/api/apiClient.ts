baf73e9d15ac651aa4a3b367501a2ea316ad8300
Raj <raj3373y@gmail.com>
2026-02-19 19:53:52 +0530

    Api client for validating all requests sent by frontend

---
+++ lib/api/apiClient.ts
@@ -0,0 +1,23 @@
+import axios, { Axios, AxiosError, InternalAxiosRequestConfig } from "axios"
+import { tokenStore } from "./tokenStore"
+
+
+// Axios instance of baseUrl and Json defaults
+export const api = axios.create({
+    baseURL: "http://192.168.1.13:3000",
+    timeout: 15000,
+    headers: {"Content-Type": "application/json"}
+})
+
+// Request interceptor that auto attachs token to every request
+axios.interceptors.request.use(async (config: InternalAxiosRequestConfig) => {
+    const token = await tokenStore.get()
+    if(token) config.headers.Authorization = `Bearer ${token}`
+    return config
+})
+
+//error message extractor so won't deal with Axios internals
+export function getApiErrorMessage(err: unknown) {
+    const e = err as AxiosError<any>;
+    return e?.response?.data?.message || e?.message || "Something went wrong" 
+}
