2eec709c0dee8a223a032bd1717da826209e4aa7
Raj <raj3373y@gmail.com>
2026-02-27 22:39:17 +0530

    Created a component button for Adding game

---
+++ components/AddGameCard.tsx
@@ -0,0 +1,45 @@
+import React from "react";
+import { Pressable, StyleSheet, Text, View } from "react-native";
+
+/*
+* Reusable Add Game Card.
+* This keeps creation trigger seperate from gameCard logic 
+*/
+type Props = {
+    onPress: () => void;
+}
+
+export default function AddGameCard({onPress}: Props) {
+    return (
+        <Pressable style={styles.card}>
+            <View style={styles.center}>
+                <Text style={styles.plus}>+</Text>
+                <Text style={styles.label}>Add Game</Text>
+            </View>
+        </Pressable>
+    )
+}
+
+
+const styles = StyleSheet.create({
+    card: {
+        borderWidth: 1,
+        borderColor: "#2a2a2a",
+        borderRadius: 14,
+        padding: 20,
+        marginBottom: 12,
+        alignItems: "center",
+        justifyContent: "center"
+    },
+    center: {
+        alignItems: "center"
+    }, 
+    plus: {
+        fontSize: 26,
+        fontWeight: "700"
+    },
+    label: {
+        marginTop: 4,
+        opacity: 0.7
+    }
+})
