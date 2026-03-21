5ab6d4baf43a690e20287af10a27af2f973b2de6
Raj <raj3373y@gmail.com>
2026-03-21 12:17:18 +0530

    work: useContext for the unlock rules related states

---
+++ context/UnlockContext.tsx
@@ -0,0 +1,69 @@
+import { api } from "@/lib/api/apiClient";
+import { createContext, ReactNode, useContext, useEffect, useState } from "react";
+
+type UnlockData = {
+    availableMinutes: number,
+    earnedMinutes: number,
+    usedMinutes: number
+}
+
+type UnlockRule = {
+    minutes_per_task: number,
+    daily_limit_minutes: number | null
+}
+
+type unlockDataContext = {
+    unlockData: UnlockData | null,
+    rule: UnlockRule | null,
+    loading: boolean,
+    refreshUnlock: () => Promise<void>
+}
+
+type Props = {
+    children: React.ReactNode;
+}
+
+const UnlockContext = createContext<unlockDataContext | null>(null);
+
+export function UnlockProvider ({children}: Props) {
+    const [unlockData, setUnlockData] = useState<UnlockData | null>(null);
+    const [rule, setRule] = useState<UnlockRule | null>(null)
+    const [loading, setLoading] = useState(true);
+
+    const fetchUnlockData = async () => {
+        try {
+            setLoading(true)
+            const [ruleRes, dataRes] = await Promise.all ([
+                api.get("/unlock-rules"),
+                api.get(`/unlock-rules/available-time`)
+            ])
+            setRule(ruleRes.data);
+            setUnlockData(dataRes.data);
+        } catch(err) {
+            console.log("Unlock fetch error: ", err)
+        } finally {
+            setLoading(false)
+        }
+    }
+
+    useEffect(() => {
+        fetchUnlockData();
+    }, [])
+
+    return (
+        <UnlockContext.Provider value={{
+            unlockData,
+            rule,
+            loading,
+            refreshUnlock: fetchUnlockData
+        }}>
+            {children}
+        </UnlockContext.Provider>
+    )
+}
+
+export function useUnlock() {
+    const context = useContext(UnlockContext);
+    if(!context) throw new Error("useUnlock must be used inside Unlock Provider")
+    return context;
+}
