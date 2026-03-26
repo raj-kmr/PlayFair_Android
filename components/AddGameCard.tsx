e515c5dcc67add5be32fbb484517424e07cc72e3
Raj <raj3373y@gmail.com>
2026-03-26 17:41:15 +0530

    work: implemented tailwind design in component

--- components/AddGameCard.tsx
+++ components/AddGameCard.tsx
@@ -7,45 +7,25 @@ import { Pressable, Text, View, StyleSheet } from "react-native";
 * This keeps creation trigger seperate from gameCard logic 
 */
 type Props = {
-    onPress: () => void;
-}
+  onPress: () => void;
+};
 
-export default function AddGameCard({onPress}: Props) {
-    return (
-        <Pressable 
-            style={({pressed}) => [styles.card, pressed && {opacity: 0.7}]}
-            onPress={onPress}
-        >
-            <View style={styles.center}>
-                <Text style={styles.plus}>+</Text>
-                <Text className=" text-emerald-700 bg-red-600" style={styles.label}>Add Game</Text>
-            </View>
-        </Pressable>
-    )
-}
+export default function AddGameCard({ onPress }: Props) {
+  return (
+<Pressable
+  onPress={onPress}
+  className="w-full rounded-2xl p-4 mb-2 items-center justify-center 
+             bg-zinc-100 active:scale-[0.98] active:opacity-80"
+>
+  <View className=" items-baseline justify-center gap-2 flex flex-row">
+    <View className="w-6 h-6 rounded-full  bg-white flex items-center justify-center  shadow-sm">
+      <Text className="text-sm font-medium text-zinc-800">+</Text>
+    </View>
 
-const styles = StyleSheet.create({
-    card: {
-        borderWidth: 1,
-        borderColor: "#000",
-        borderRadius: 14,
-        padding: 20,
-        marginBottom: 12,
-        alignItems: "center",
-        justifyContent: "center",
-        backgroundColor: "#fff",
-    },
-    center: {
-        alignItems: "center",
-    }, 
-    plus: {
-        fontSize: 26,
-        fontWeight: "700",
-        color: "#000",
-    },
-    label: {
-        marginTop: 4,
-        opacity: 0.7,
-        color: "#000",
-    }
-})
+    <Text className="mt-2 text-sm font-bold text-zinc-600">
+      Add Game
+    </Text>
+  </View>
+</Pressable>
+  );
+}
